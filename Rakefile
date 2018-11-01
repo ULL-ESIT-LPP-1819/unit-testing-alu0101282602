ee0629c7bae29e660f05b86220f67b4b9b63fc91
Robin Steiger <alu0101282602@ull.edu.es>
2018-11-01 22:40:19 +0000

    Realizando primera prueba

---
+++ Rakefile
@@ -0,0 +1,6 @@
+task :default => :tu
+
+desc "Ejecutar las espectativas de la clase Nutri"
+task :tu do
+  sh "ruby -I. test/tc_file.rb"
+end
---
+++ test/tc_file.rb
@@ -0,0 +1,7 @@
+require 'bin/file'
+require 'test/unit'
+
+#Pruebas unitarias
+#Aqui se realizan las pruebas para comprobar que la clase funciona
+class TestNutrition < Test::Unit::TestCase
+end
